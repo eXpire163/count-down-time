5a1cc5f8ad7f1a997adee54464122096a6e99ed8
Christian Wiedemann-Gruber <christian.wiedemann@capgemini.com>
2023-04-14 14:59:37 +0200

    meeting timer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+
+
+const calculateTimeLeft = (endHour, endMinute) => {
+  let year = new Date().getFullYear();
+  let month = new Date().getMonth();
+  let day = new Date().getDate();
+  let difference = +new Date(year, month, day, endHour, endMinute) - +new Date();
+  //console.log(endHour, endMinute)
+  let timeLeft = {};
+
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60)
+    };
+  }
+
+  return timeLeft;
+}
+
 
 function App() {
+
+
+  const searchParams = new URLSearchParams(document.location.search)
+
+  const endHour = searchParams.get('endHour');
+  const endMinute = searchParams.get('endMinute');
+  const topic = searchParams.get('topic');;
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(endHour, endMinute));
+
+  const timerComponents = [];
+
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft(endHour, endMinute));
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!endHour && !endMinute ? <><h1>start via url</h1>
+          <a className="App-link" href='?endHour=15&endMinute=50'>without topic</a>
+          <a className="App-link" href='?endHour=15&endMinute=50&topic=mytimer'>withtopic</a></> : <></>}
+        {topic?.length ? <h1>{topic}</h1> : <></>}
+        {timerComponents.length ? <h1>Start in {timerComponents}</h1> : <h1>let's go</h1>}
       </header>
-    </div>
+    </div >
   );
+
 }
 
 export default App;
